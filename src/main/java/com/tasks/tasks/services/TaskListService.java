677e45c030b59e5a08fede2181ce688e959b87e4
Mario Liebenberg <mario.liebenberg@hotmail.com>
2025-10-30 14:48:49 +0200

    TaskList-service+controller

---
+++ src/main/java/com/tasks/tasks/services/TaskListService.java
@@ -0,0 +1,9 @@
+package com.tasks.tasks.services;
+
+import com.tasks.tasks.domain.entities.TaskList;
+
+import java.util.List;
+
+public interface TaskListService {
+    List<TaskList> listTaskLists();
+}
---
+++ src/main/java/com/tasks/tasks/services/impl/TaskListServiceImpl.java
@@ -0,0 +1,23 @@
+package com.tasks.tasks.services.impl;
+
+import com.tasks.tasks.domain.entities.TaskList;
+import com.tasks.tasks.repositories.TaskListRepository;
+import com.tasks.tasks.services.TaskListService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskListServiceImpl implements TaskListService {
+
+    private final TaskListRepository taskListRepository;
+
+    public TaskListServiceImpl(TaskListRepository taskListRepository) {
+        this.taskListRepository = taskListRepository;
+    }
+
+    @Override
+    public List<TaskList> listTaskLists() {
+        return taskListRepository.findAll();
+    }
+}
